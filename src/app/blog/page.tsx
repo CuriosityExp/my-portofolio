578a3ac3f6cdd03e3a2c6130591bd01cc69bdeca
CuriosityExp <dsianturi57@gmail.com>
2023-08-16 22:44:58 +0700

    chore: add development warning to blog and portfolio page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import {IoWarningOutline} from "react-icons/io5"
 export default function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <IoWarningOutline size={100} color={"#f19b22"} />
+      <h1 className='text-6xl'>
+        The following page is currently under development.
+      </h1>
+    </div>
   )
 }
