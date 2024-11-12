6274c4944706b15a19999cd40fdf2e6da65f33e2
Daniel Elisandro <dsianturi57@.gmail.com>
2024-11-12 15:49:46 +0700

    chore: add meta opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Daniel Elisandro",
   description: "Web Profile for Daniel Elisandro created using Nextjs",
+  openGraph: {
+    type: "website",
+    url: "https://danielelisandro.vercel.app/about",
+    title: "Daniel Elisandro - Full Stack Developer",
+    description: "Tech-savvy individual with a strong foundation in physics, specializing in web development and mobile applications.",
+    images: [
+      {
+        url: "https://danielelisandro.vercel.app/_next/image?url=%2Fdanielelisandro.png&w=750&q=75",
+        width: 800,
+        height: 600,
+        alt: "Profile Image of Daniel Elisandro",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
