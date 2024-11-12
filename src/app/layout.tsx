ad6cf6e34b3d0c2a0fa118601c093579347707ee
Daniel Elisandro <dsianturi57@.gmail.com>
2024-11-12 16:13:00 +0700

    chore: change tags type to article

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,43 @@
-import BottomTab from "@/components/BottomTab";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+  import BottomTab from "@/components/BottomTab";
+  import "./globals.css";
+  import type { Metadata } from "next";
+  import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+  const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Daniel Elisandro",
-  description: "Web Profile for Daniel Elisandro created using Nextjs",
-  openGraph: {
-    type: "website",
-    url: "https://danielelisandro.vercel.app/about",
-    title: "Daniel Elisandro - Full Stack Developer",
-    description: "Tech-savvy individual with a strong foundation in physics, specializing in web development and mobile applications.",
-    images: [
-      {
-        url: "https://danielelisandro.vercel.app/_next/image?url=%2Fdanielelisandro.png&w=750&q=75",
-        width: 800,
-        height: 600,
-        alt: "Profile Image of Daniel Elisandro",
-      },
-    ],
-  },
-};
+  export const metadata: Metadata = {
+    title: "Daniel Elisandro",
+    description: "Web Profile for Daniel Elisandro created using Nextjs",
+    openGraph: {
+      type: "article",
+      url: "https://danielelisandro.vercel.app/about",
+      title: "Daniel Elisandro - Full Stack Developer",
+      description: "Tech-savvy individual with a strong foundation in physics, specializing in web development and mobile applications.",
+      images: [
+        {
+          url: "https://danielelisandro.vercel.app/_next/image?url=%2Fdanielelisandro.png&w=750&q=75",
+          width: 800,
+          height: 600,
+          alt: "Profile Image of Daniel Elisandro",
+        },
+      ],
+    },
+  };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex flex-col">
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex flex-col">
 
-        {children}
-        <BottomTab />
-        </div>
-      </body>
-    </html>
-  );
-}
+          {children}
+          <BottomTab />
+          </div>
+        </body>
+      </html>
+    );
+  }
